3bb456827402f8eb6f34daf331b683adbc4f3c0a
Dhruv Srivastav <dhruv2204@gmail.com>
2023-02-28 20:08:20 +0530

    fix(server): url fix

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import {CodegenConfig} from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: 'http://localhost:4000/graphql/',
+  schema: 'https://movieparadiseo.herokuapp.com/graphql/',
   documents: ['src/**/*.tsx'],
   generates: {
     './src/__generated__/': {
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from './pages/App';
 import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql/',
+  uri: 'https://movieparadiseo.herokuapp.com/graphql/',
   cache: new InMemoryCache(),
 });
 
