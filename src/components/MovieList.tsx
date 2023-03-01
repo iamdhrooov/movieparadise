4a44128439821f2ab9c6a119cb1642120ec5f0c8
Dhruv Srivastav <dhruv2204@gmail.com>
2023-03-01 17:53:02 +0530

    fix(css): minor

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -5,7 +5,7 @@ import MovieCard from './MovieCard';
 const MovieList = ({popularMovies}) => {
   const history = useHistory();
   return (
-    <ul>
+    <ul style={{maxWidth: 1650, margin: '0 auto'}}>
       {popularMovies.map((movie: Paradise.MovieCardInfo) => (
         <li
           style={{float: 'left', margin: '1em', cursor: 'pointer'}}
